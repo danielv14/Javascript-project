863c002d261b3cab45f4f2a711929c44acc6cac5
Daniel V <daniel.vernberg@gmail.com>
2015-01-06 15:19:58 +0100

    Init the repo
    First commit with skeleton bootstrap layout

---
+++ footer.php
@@ -0,0 +1,14 @@
+  <div class = "navbar navbar-default navbar-fixed-bottom">
+               
+                        <div class = "container">
+                                <p class = "navbar-text pull-left">Site Built By Daniel Vernberg</p>
+                                <a href = "#" class = "navbar-btn btn-danger btn pull-right">GitHub</a>
+                        </div>
+               
+                </div>
+ 
+                <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script src = "js/bootstrap.js"></script>
+               
+        </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include('header.php'); ?>
+ <div class = "container">
+        <div class = "jumbotron">
+                <h1>Hello, World!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+                <a class = "btn btn-default">Watch Now!</a>
+                <a class = "btn btn-info">Tweet it!</a>
+         </div>
+</div>
+<?php include('footer.php'); ?>
+
+
+ 
+              
